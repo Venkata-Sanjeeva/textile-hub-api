dd284e1a40db1913bcb65965376641ce41577807
Venkata Sanjeeva Kuntumalla <152136689+Venkata-Sanjeeva@users.noreply.github.com>
2026-01-20 21:53:32 +0530

    Refactor AdminDashboardProjectApplication class
    Removed CommandLineRunner implementation and user initialization logic.

--- src/main/java/com/example/adminDashboardProject/AdminDashboardProjectApplication.java
+++ src/main/java/com/example/adminDashboardProject/AdminDashboardProjectApplication.java
@@ -1,35 +1,11 @@
 package com.example.adminDashboardProject;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.example.adminDashboardProject.entity.User;
-import com.example.adminDashboardProject.entity.UserType;
-import com.example.adminDashboardProject.repository.UserRepository;
-
 @SpringBootApplication
-public class AdminDashboardProjectApplication implements CommandLineRunner{
-
-	@Autowired
-	private UserRepository userRepo;
-	
+public class AdminDashboardProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AdminDashboardProjectApplication.class, args);
 	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		if(userRepo.findByUsername("sanjeeva") == null) {
-			User user = new User();
-		
-			user.setUsername("sanjeeva");
-			user.setPassword("VS@86@kv");
-			user.setUserType(UserType.ADMIN);
-			
-			userRepo.save(user);
-		}
-	}
-
 }
